a4ef99eb4d862daa8337656b782d7d5efaafbf3a
Cameron Braid <cameron@drivenow.com.au>
2018-06-27 22:29:38 +1000

    support mobx state management

--- src/components/defineComponent.js
+++ src/components/defineComponent.js
@@ -4,7 +4,7 @@
 var BaseState = require("./State");
 var BaseComponent = require("./Component");
 var inherit = require("raptor-util/inherit");
-
+var mobx = require("mobx");
 module.exports = function defineComponent(def, renderer) {
     if (def.___isComponent) {
         return def;
@@ -25,6 +25,60 @@ module.exports = function defineComponent(def, renderer) {
 
     ComponentClass.prototype = proto;
 
+    if (proto.mobxObservable) {
+        let MODE_STATE_CHANGE = "state-change";
+        let MODE_FORCE_UPDATE = "force-update";
+        let MODE_FORCE_UPDATE_UPDATE = "force-update-update";
+        let UPDATE_MODE = MODE_STATE_CHANGE;
+
+        let onDestroy = proto.onDestroy;
+        proto.onDestroy = function() {
+            this.$mobx && this.$mobx.dispose();
+            delete this.$mobx;
+            if (onDestroy) onDestroy.apply(this, arguments);
+        };
+        let onCreate = proto.onCreate;
+        proto.onCreate = function() {
+            // if (this.mobxObservable())
+            // console.log("mobxObservable", this.mobxObservable());
+
+            this.$mobx = new mobx.Reaction("reaction-" + this.id, () => {
+                // let o = this.mobxObservable();
+                // if (o) console.log("mobxObservable-forceUpdate", this);
+                // if (o == "debugger") debugger;
+
+                if (UPDATE_MODE == MODE_STATE_CHANGE) {
+                    this.state.___mobx++;
+                } else if (UPDATE_MODE == MODE_FORCE_UPDATE) {
+                    this.forceUpdate();
+                } else if (UPDATE_MODE == MODE_FORCE_UPDATE_UPDATE) {
+                    this.forceUpdate();
+                    this.update();
+                }
+            });
+
+            if (onCreate) {
+                onCreate.apply(this, arguments);
+            }
+
+            if (UPDATE_MODE == MODE_STATE_CHANGE) {
+                if (!this.state) {
+                    this.state = {
+                        ___mobx: 0
+                    };
+                } else {
+                    this.state.___set("___mobx", 0, true, false);
+                }
+            }
+        };
+        proto.___mobx_render = function(renderFunc) {
+            this.$mobx.track(() => {
+                mobx._allowStateChanges(false, () => {
+                    renderFunc();
+                });
+            });
+        };
+    }
     // We don't use the constructor provided by the user
     // since we don't invoke their constructor until
     // we have had a chance to do our own initialization.
