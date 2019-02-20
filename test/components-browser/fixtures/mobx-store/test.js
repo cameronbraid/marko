19d5fc5ea31dd08caaad61822aca64876533eb74
Cameron Braid <cameron@drivenow.com.au>
2019-02-21 01:59:53 +1100

    add dom checking to mobx test

--- test/components-browser/fixtures/mobx-store/test.js
+++ test/components-browser/fixtures/mobx-store/test.js
@@ -4,7 +4,6 @@ const Store = require("./store").Store;
 
 module.exports = function(helpers) {
     var app = helpers.mount(require.resolve("./index"), {});
-
     var viewOne = window.viewOne;
     var viewTwo = window.viewTwo;
 
@@ -15,27 +14,39 @@ module.exports = function(helpers) {
     }
 
     expect(app.renderCount).to.equal(1);
+    expect(app.getEl("count").innerHTML).to.equal("0");
     expect(viewOne.renderCount).to.equal(1);
+    expect(viewOne.getEl("value").innerHTML).to.equal("0");
     expect(viewTwo.renderCount).to.equal(1);
+    expect(viewTwo.getEl("value").innerHTML).to.equal("0");
 
     Store.increment();
     update();
 
     expect(app.renderCount).to.equal(2);
+    expect(app.getEl("count").innerHTML).to.equal("1");
     expect(viewOne.renderCount).to.equal(1);
+    expect(viewOne.getEl("value").innerHTML).to.equal("0");
     expect(viewTwo.renderCount).to.equal(1);
+    expect(viewTwo.getEl("value").innerHTML).to.equal("0");
 
     Store.valueOne++;
     update();
 
     expect(app.renderCount).to.equal(2);
+    expect(app.getEl("count").innerHTML).to.equal("1");
     expect(viewOne.renderCount).to.equal(2);
+    expect(viewOne.getEl("value").innerHTML).to.equal("1");
     expect(viewTwo.renderCount).to.equal(1);
+    expect(viewTwo.getEl("value").innerHTML).to.equal("0");
 
     Store.valueTwo++;
     update();
 
     expect(app.renderCount).to.equal(2);
+    expect(app.getEl("count").innerHTML).to.equal("1");
     expect(viewOne.renderCount).to.equal(2);
+    expect(viewOne.getEl("value").innerHTML).to.equal("1");
     expect(viewTwo.renderCount).to.equal(2);
+    expect(viewTwo.getEl("value").innerHTML).to.equal("1");
 };
