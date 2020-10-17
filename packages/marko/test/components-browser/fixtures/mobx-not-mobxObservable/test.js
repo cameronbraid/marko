f8618decc4c330a731ef64cd58987e13269c89ee
Cameron Braid <cameron@drivenow.com.au>
2020-10-17 23:07:49 +1100

    test(mobx): mobx tests

---
+++ packages/marko/test/components-browser/fixtures/mobx-not-mobxObservable/test.js
@@ -0,0 +1,12 @@
+var expect = require("chai").expect;
+
+module.exports = function(helpers) {
+  var app = helpers.mount(require.resolve("./index"), {});
+  var countEl = app.getEl("count");
+  expect(countEl.innerHTML).to.equal("0");
+
+  app.increment();
+  app.update();
+
+  expect(countEl.innerHTML).to.equal("0");
+};
